d0cb19d15c941c5b85848a735b3f1bff8d1f2505
Georg Eckert <eckert.georg@gmx.de>
2015-12-17 01:21:58 +0100

    UI battle improvement
    Former-commit-id: dac62c47990fec591fc4c758af9da867b03977d1

---
+++ FriendsWithMonsters/core/src/org/limbusdev/monsterworld/ecs/components/TeamComponent.java
@@ -0,0 +1,21 @@
+package org.limbusdev.monsterworld.ecs.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.utils.Array;
+
+import org.limbusdev.monsterworld.model.Monster;
+
+/**
+ * Created by georg on 16.12.15.
+ */
+public class TeamComponent implements Component {
+    /* ............................................................................ ATTRIBUTES .. */
+    // Only 3 monsters may accompany the hero, they can be switched with other monsters at
+    // Summoning Points
+    public Array<Monster> monsters;
+    /* ........................................................................... CONSTRUCTOR .. */
+    
+    /* ............................................................................... METHODS .. */
+    
+    /* ..................................................................... GETTERS & SETTERS .. */
+}

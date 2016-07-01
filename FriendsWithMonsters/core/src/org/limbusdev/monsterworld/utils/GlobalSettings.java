85f203e4f43992be775eeb04d3f04e9d6e819a35
Georg Eckert <eckert.georg@gmx.de>
2016-07-01 19:07:43 +0200

    adds dpad
    Former-commit-id: e7b2f73aa44922a207bbeec1bed99e2b0ecbc054

--- FriendsWithMonsters/core/src/org/limbusdev/monsterworld/utils/GlobalSettings.java
+++ FriendsWithMonsters/core/src/org/limbusdev/monsterworld/utils/GlobalSettings.java
@@ -8,11 +8,11 @@ public class GlobalSettings {
     /* ............................................................................ ATTRIBUTES .. */
     public static final int TILE_SIZE = 16;
     public static final int PIXELS_PER_METER = 1;
-    public static final int RESOLUTION_X = 800;
-    public static final int RESOLUTION_Y = 480;
+    public static final int RESOLUTION_X = 1280;
+    public static final int RESOLUTION_Y = 720;
     public static final int ONE_STEPDURATION_MS = 5;
     public static final int ONE_STEP_DURATION_PERSON = 20;
-    public static final int zoom = 2;
+    public static final int zoom = 3;
     public static final boolean DEBUGGING_ON = false;
     public static final int MONSTER_SPRITES = 22;
     public static final boolean JoyStick = true;

eef88815bb595289d21be8e8013d8839ba5e611e
Georg Eckert <eckert.georg@gmx.de>
2017-02-01 21:33:30 +0100

    new animation system
    Former-commit-id: 328876cbb35ce9dbf0397c78eebdc22da22c819b

--- GuardianMonsters/core/src/de/limbusdev/guardianmonsters/fwmengine/world/ui/OrthogonalTiledMapAndEntityRenderer.java
+++ GuardianMonsters/core/src/de/limbusdev/guardianmonsters/fwmengine/world/ui/OrthogonalTiledMapAndEntityRenderer.java
@@ -14,14 +14,11 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ArrayMap;
 import com.badlogic.gdx.utils.Sort;
-import com.sun.media.sound.AiffFileReader;
 
 import de.limbusdev.guardianmonsters.data.TextureAssets;
 import de.limbusdev.guardianmonsters.geometry.IdentifiableRectangle;
 import de.limbusdev.guardianmonsters.fwmengine.managers.Media;
 import de.limbusdev.guardianmonsters.fwmengine.managers.Services;
-import de.limbusdev.guardianmonsters.geometry.IntRectangle;
-import de.limbusdev.guardianmonsters.geometry.IntVector2;
 
 
 /**
@@ -37,6 +34,7 @@ public class OrthogonalTiledMapAndEntityRenderer extends OrthogonalTiledMapRende
     private Media media;
     private float elapsedTime;
     private Array<Vector2> weatherTiles;
+    private boolean weatherOn;
 
     /* ........................................................................... CONSTRUCTOR .. */
     public OrthogonalTiledMapAndEntityRenderer(
@@ -47,14 +45,8 @@ public class OrthogonalTiledMapAndEntityRenderer extends OrthogonalTiledMapRende
         this.animationTextures = new ArrayMap<Integer,TextureRegion>();
         this.animatedObjectsTextures = new ArrayMap<Integer, TextureRegion>();
         this.objectAnimations = new ArrayMap<String,Animation>();
-        weatherTiles = new Array<Vector2>();
-        int mapWidth = map.getProperties().get("width", Integer.class)*16;
-        int mapHeight = map.getProperties().get("height", Integer.class)*16;
-        for(int i=-1; i<mapWidth/640+1; i++) {
-            for(int j=-1; j<mapHeight/480+1; j++) {
-                weatherTiles.add(new Vector2(i*640,j*480));
-            }
-        }
+        this.weatherOn = map.getProperties().containsKey("weather");
+        if(weatherOn) setUpWeather(map);
         this.media = Services.getMedia();
         this.elapsedTime = 0;
     }
@@ -76,22 +68,7 @@ public class OrthogonalTiledMapAndEntityRenderer extends OrthogonalTiledMapRende
         for(MapLayer layer : map.getLayers()) {
             if(layer instanceof TiledMapTileLayer) {
                 renderTileLayer((TiledMapTileLayer) layer);
-                currentLayer++;
-                if(layer.getName().equals("objects1")) {
 
-                    if(map.getLayers().get("animatedObjects") != null) {
-                        MapLayer animObjLayer = map.getLayers().get("animatedObjects");
-                        renderAnimatedObjects(animObjLayer);
-                    }
-
-                    // Draw entity sprites if visible
-                    for(EntitySprite es : sprites) {
-                        if (es.visible) {
-                            es.draw(this.batch);
-                        }
-                    }
-
-                }
                 if(layer.getName().equals("ground1")) {
                     renderAnimatedTiles();
                 }
@@ -99,15 +76,37 @@ public class OrthogonalTiledMapAndEntityRenderer extends OrthogonalTiledMapRende
             } else {
                 for (MapObject object : layer.getObjects())
                     renderObject(object);
+
+                if(layer.getName().equals("people")) {
+                    // Draw entity sprites if visible
+                    for(EntitySprite es : sprites) {
+                        if (es.visible) {
+                            es.draw(this.batch);
+                        }
+                    }
+                }
             }
         }
 
         // Render Weather Effects
-        drawWeather();
+        if(weatherOn)drawWeather();
 
 
         endRender();
     }
+
+    @Override
+    public void renderObject(MapObject object) {
+        String objName = object.getName();
+
+        // Don't render objects with empty name
+        if(objName == null) return;
+
+        if(objName.equals("animatedObject")) {
+            renderAnimatedObject(object);
+        }
+    }
+
     /* ..................................................................... GETTERS & SETTERS .. */
     public void addEntitySprite(EntitySprite es) {
         sprites.add(es);
@@ -142,9 +141,12 @@ public class OrthogonalTiledMapAndEntityRenderer extends OrthogonalTiledMapRende
     public void setUpAnimatedObjects(MapLayer mapLayer) {
         try {
             for (MapObject mo : mapLayer.getObjects()) {
-                RectangleMapObject r = (RectangleMapObject) mo;
                 String id = mo.getProperties().get("index", String.class);
                 Animation a = media.getObjectAnimation(id);
+                if(mo.getProperties().containsKey("frameDuration")){
+                    String frmDur = mo.getProperties().get("frameDuration", String.class);
+                    a.setFrameDuration(Float.parseFloat(frmDur));
+                }
                 objectAnimations.put(id,a);
             }
         } catch (Exception e) {
@@ -160,13 +162,22 @@ public class OrthogonalTiledMapAndEntityRenderer extends OrthogonalTiledMapRende
             }
     }
 
-    private void renderAnimatedObjects(MapLayer mapLayer) {
-        for(MapObject o : mapLayer.getObjects()) {
+    private void renderAnimatedObject(MapObject o) {
             RectangleMapObject r = (RectangleMapObject) o;
             this.batch.draw(
                 objectAnimations.get(o.getProperties().get("index", String.class)).getKeyFrame(elapsedTime),
                 r.getRectangle().getX(),
                 r.getRectangle().getY());
+    }
+
+    private void setUpWeather(TiledMap map) {
+        weatherTiles = new Array<Vector2>();
+        int mapWidth = map.getProperties().get("width", Integer.class)*16;
+        int mapHeight = map.getProperties().get("height", Integer.class)*16;
+        for(int i=-1; i<mapWidth/640+1; i++) {
+            for(int j=-1; j<mapHeight/480+1; j++) {
+                weatherTiles.add(new Vector2(i*640,j*480));
+            }
         }
     }
 

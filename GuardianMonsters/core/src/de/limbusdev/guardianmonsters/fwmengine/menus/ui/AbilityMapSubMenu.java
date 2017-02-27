29268aff94f90bceb1411263c7130bac60a6d45f
Georg Eckert <eckert.georg@gmx.de>
2017-02-27 19:50:49 +0100

    m
    Former-commit-id: 58aa0416fd5ea5e831a8f2b0c6d59b828fff8884

--- GuardianMonsters/core/src/de/limbusdev/guardianmonsters/fwmengine/menus/ui/AbilityMapSubMenu.java
+++ GuardianMonsters/core/src/de/limbusdev/guardianmonsters/fwmengine/menus/ui/AbilityMapSubMenu.java
@@ -1,20 +1,27 @@
 package de.limbusdev.guardianmonsters.fwmengine.menus.ui;
 
 import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ArrayMap;
 
+import de.limbusdev.guardianmonsters.data.BundleAssets;
+import de.limbusdev.guardianmonsters.fwmengine.managers.Services;
 import de.limbusdev.guardianmonsters.model.AbilityGraph;
 import de.limbusdev.guardianmonsters.model.Monster;
 import de.limbusdev.guardianmonsters.model.MonsterInfo;
+import de.limbusdev.guardianmonsters.model.MonsterStatusInformation;
 import de.limbusdev.guardianmonsters.utils.GS;
 
+import static de.limbusdev.guardianmonsters.model.AbilityGraph.HORIZONTAL;
 import static de.limbusdev.guardianmonsters.model.AbilityGraph.UPLEFT;
 import static de.limbusdev.guardianmonsters.model.AbilityGraph.UPRIGHT;
 import static de.limbusdev.guardianmonsters.model.AbilityGraph.VERTICAL;
@@ -26,15 +33,34 @@ import static de.limbusdev.guardianmonsters.model.AbilityGraph.VERTICAL;
 
 public class AbilityMapSubMenu extends AInventorySubMenu {
 
+    private enum NodeStatus {
+        DISABLED, ENABLED, ACTIVATED,
+    }
+
+    private ButtonGroup fieldGroup;
     private Group connections, circles;
     private ArrayMap<Integer,ImageButton> nodeButtons;
     private ArrayMap<AbilityGraph.Edge,Image> edgeImgs;
     private ArrayMap<Integer, Monster> team;
     private AbilityGraph graph;
+    private TeamCircleWidget circleWidget;
+    private Label fieldDescription, remainingLevels;
+    private Button learn;
+    private ImageButton.ImageButtonStyle disabled, enabled, activated;
+    private ImageButton.ImageButtonStyle disabledAbility, enabledAbility, activatedAbility;
+    private String[] edgeImgsDisabled = {"graph-horizontal","graph-vertical","graph-upleft","graph-upright"};
 
-    public AbilityMapSubMenu(Skin skin, ArrayMap<Integer,Monster> team) {
+    public AbilityMapSubMenu(Skin skin, ArrayMap<Integer,Monster> teamMonsters) {
         super(skin);
-        this.team = team;
+        this.team = teamMonsters;
+        this.disabled  = skin.get("board-disabled",  ImageButton.ImageButtonStyle.class);
+        this.enabled   = skin.get("board-enabled",   ImageButton.ImageButtonStyle.class);
+        this.activated = skin.get("board-activated", ImageButton.ImageButtonStyle.class);
+        this.disabledAbility  = skin.get("board-ability-disabled",  ImageButton.ImageButtonStyle.class);
+        this.enabledAbility   = skin.get("board-ability-enabled",   ImageButton.ImageButtonStyle.class);
+        this.activatedAbility = skin.get("board-ability-activated", ImageButton.ImageButtonStyle.class);
+
+        // Initial Setup
         Group container = new Group();
         container.setSize(1200,600);
         connections = new Group();
@@ -46,33 +72,11 @@ public class AbilityMapSubMenu extends AInventorySubMenu {
 
         edgeImgs = new ArrayMap<>();
         graph = new AbilityGraph();
-        graph.getEdges();
 
-        for(AbilityGraph.Edge e : graph.getEdges()) {
-            AbilityGraph.Vertex curr = e.from;
-            AbilityGraph.Vertex next = e.to;
-
-            addConnection(skin, curr, next, e);
-        }
-
-        ButtonGroup btnGrp = new ButtonGroup();
-        btnGrp.setMinCheckCount(1);
-        btnGrp.setMaxCheckCount(1);
+        fieldGroup = new ButtonGroup();
+        fieldGroup.setMinCheckCount(1);
+        fieldGroup.setMaxCheckCount(1);
         nodeButtons = new ArrayMap<>();
-        for(AbilityGraph.Vertex v : graph.getVertices().values()) {
-            // Add Circle
-            ImageButton circle;
-            if(MonsterInfo.getInstance().getStatusInfos().get(team.get(0).ID).learnableAttacks.containsKey(v.ID)) {
-                circle = new ImageButton(skin,"button-board-node-ability-disabled");
-            } else {
-                circle = new ImageButton(skin,"button-board-node-disabled");
-            }
-            circle.setPosition(600/2-8+32*v.x,300/2-8+32*v.y, Align.bottomLeft);
-            circles.addActor(circle);
-            nodeButtons.put(v.ID, circle);
-            btnGrp.add(circle);
-        }
-        nodeButtons.get(0).setChecked(true);
 
         container.addActor(connections);
         container.addActor(circles);
@@ -86,45 +90,107 @@ public class AbilityMapSubMenu extends AInventorySubMenu {
         scrollPane.setScrollPercentY(.5f);
         addActor(scrollPane);
 
-        refreshDisplay();
+        TeamCircleWidget.ClickHandler clh = new TeamCircleWidget.ClickHandler() {
+            @Override
+            public void onTeamMemberButton(int position) {
+                initBoard(team.get(position));
+            }
+        };
+
+        Group teamWidget = new Group();
+        teamWidget.setSize(143,200);
+        Label circleBg = new Label("", skin, "list-item");
+        circleBg.setSize(143,200);
+        circleBg.setPosition(0,0,Align.bottomLeft);
+        circleWidget = new TeamCircleWidget(skin, teamMonsters, clh);
+        circleWidget.setPosition(1,48,Align.bottomLeft);
+        teamWidget.addActor(circleBg);
+        teamWidget.addActor(circleWidget);
+        teamWidget.setPosition(2,2,Align.bottomLeft);
+        addActor(teamWidget);
+
+        fieldDescription = new Label("Test", skin, "default");
+        fieldDescription.setSize(120,32);
+        fieldDescription.setPosition(3,7,Align.bottomLeft);
+        fieldDescription.setAlignment(Align.topLeft,Align.topLeft);
+        teamWidget.addActor(fieldDescription);
+
+        learn = new ImageButton(skin, "button-learn");
+        learn.setPosition(140-33,7,Align.bottomLeft);
+        teamWidget.addActor(learn);
+
+        Image lvlImg = new Image(skin.getDrawable("stats-symbol-exp"));
+        lvlImg.setSize(16,16);
+        lvlImg.setPosition(4,44,Align.bottomLeft);
+        teamWidget.addActor(lvlImg);
+
+        remainingLevels = new Label("0", skin, "default");
+        remainingLevels.setPosition(21,44,Align.bottomLeft);
+        teamWidget.addActor(remainingLevels);
+
+        initBoard(team.get(0));
 
         //if(GS.DEBUGGING_ON) setDebug(true, true);
 
     }
 
-    private void addConnection(Skin skin, AbilityGraph.Vertex from, AbilityGraph.Vertex to, AbilityGraph.Edge edge) {
+    private void addConnection(Skin skin, AbilityGraph.Edge edge) {
         Image bar = null;
-        if(from.x != to.x && from.y == to.y) {
-            // Horizontal
+        String imgName = edgeImgsDisabled[edge.orientation];
+
+        int align;
+        switch(edge.orientation) {
+            case HORIZONTAL:    align = Align.left; break;
+            case VERTICAL:      align = Align.bottom; break;
+            case UPLEFT:        align = Align.bottomRight; break;
+            default:            align = Align.bottomLeft; break;
+        }
+
+        // Sort from down left to up right
+        AbilityGraph.Vertex from, to;
+        if(edge.orientation == HORIZONTAL || edge.orientation == UPRIGHT) {
+            if(edge.from.x < edge.to.x) {
+                from = edge.from;
+                to = edge.from;
+            } else {
+                from = edge.to;
+                to = edge.from;
+            }
+        } else {
+            if(edge.from.y < edge.to.y) {
+                from = edge.from;
+                to = edge.from;
+            } else {
+                from = edge.to;
+                to = edge.from;
+            }
+        }
+
+        if(edge.orientation == HORIZONTAL) {
             int fx, tx;
             fx = from.x < to.x ? from.x : to.x;
             tx = from.x < to.x ? to.x : from.x;
 
             for(int x = fx; x < tx; x++) {
-                bar = new Image(skin.getDrawable("ability-connection-hor"));
+                bar = new Image(skin.getDrawable(imgName));
                 bar.setPosition(600/2+x*32, 300/2-16+from.y*32, Align.bottomLeft);
                 connections.addActor(bar);
             }
         }
 
-        if(from.y != to.y && from.x == to.x) {
-            // Vertical
+        if(edge.orientation == VERTICAL) {
             int fy, ty;
             fy = from.y < to.y ? from.y : to.y;
             ty = from.y < to.y ? to.y : from.y;
 
             for(int y = fy; y < ty; y++) {
-                bar = new Image(skin.getDrawable("ability-connection-vert"));
+                bar = new Image(skin.getDrawable(imgName));
                 bar.setPosition(600/2+from.x*32-16, 300/2+y*32, Align.bottomLeft);
                 connections.addActor(bar);
             }
         }
 
-        if(from.y != to.y && from.x != to.x &&
-            (from.x < to.x && from.y < to.y) ||
-            (from.x > to.x && from.y > to.y)) {
-
-            // Diagonal - Up-Right
+        if(edge.orientation == UPRIGHT) {
             int fx, tx, fy, ty;
             fx = from.x < to.x ? from.x : to.x;
             tx = from.x < to.x ? to.x : from.x;
@@ -133,18 +199,15 @@ public class AbilityMapSubMenu extends AInventorySubMenu {
 
             int x=fx;
             for(int y = fy; y < ty; y++) {
-                bar = new Image(skin.getDrawable("ability-connection-diag-ur-dl"));
+                bar = new Image(skin.getDrawable(imgName));
                 bar.setPosition(600/2+x*32-4, 300/2+y*32-4, Align.bottomLeft);
                 connections.addActor(bar);
                 x++;
             }
         }
 
-        if(from.y != to.y && from.x != to.x &&
-            (from.x < to.x && from.y > to.y) ||
-            (from.x > to.x && from.y < to.y)) {
+        if(edge.orientation == UPLEFT) {
 
-            // Diagonal - Up-Right
             int fx, tx, fy, ty;
             fx = from.x < to.x ? from.x : to.x;
             tx = from.x < to.x ? to.x : from.x;
@@ -153,7 +216,7 @@ public class AbilityMapSubMenu extends AInventorySubMenu {
 
             int x = fx;
             for(int y = fy; y > ty; y--) {
-                bar = new Image(skin.getDrawable("ability-connection-diag-ul-dr"));
+                bar = new Image(skin.getDrawable(imgName));
                 bar.setPosition(600/2+x*32-4, 300/2+y*32-32-4, Align.bottomLeft);
                 connections.addActor(bar);
                 x++;
@@ -165,12 +228,28 @@ public class AbilityMapSubMenu extends AInventorySubMenu {
         }
     }
 
-    private void activateNode(int ID) {
-        if(MonsterInfo.getInstance().getStatusInfos().get(team.get(0).ID).learnableAttacks.containsKey(ID)) {
-            nodeButtons.get(ID).setStyle(getSkin().get("button-board-node-ability-activated", ImageButton.ImageButtonStyle.class));
-        } else {
-            nodeButtons.get(ID).setStyle(getSkin().get("button-board-node-activated", ImageButton.ImageButtonStyle.class));
+    /**
+     * Changes the style of the given node, according to its role and status
+     * @param ID
+     */
+    private void changeNodeStyle(int ID, NodeStatus status) {
+        Monster monster = team.get(circleWidget.getCurrentPosition());
+        MonsterStatusInformation msi = MonsterInfo.getInstance().getStatusInfos().get(monster.ID);
+
+        ImageButton.ImageButtonStyle ibs;
+        switch(status) {
+            case ACTIVATED:
+                ibs = msi.learnableAttacks.containsKey(ID) ? activatedAbility : activated;
+                break;
+            case ENABLED:
+                ibs = msi.learnableAttacks.containsKey(ID) ? enabledAbility : enabled;
+                break;
+            default: // DISABLED
+                ibs = msi.learnableAttacks.containsKey(ID) ? disabledAbility : disabled;
+                break;
         }
+
+        nodeButtons.get(ID).setStyle(ibs);
     }
 
     private void enableNeighboringNodes(int ID) {
@@ -179,37 +258,51 @@ public class AbilityMapSubMenu extends AInventorySubMenu {
             if(e.from.ID == ID || e.to.ID == ID) {
                 idToEnable = (ID == e.from.ID) ? e.to.ID : e.from.ID;
                 if(!team.get(0).activatedAbilityNodes.contains(idToEnable)) {
-                    if(MonsterInfo.getInstance().getStatusInfos().get(team.get(0).ID).learnableAttacks.containsKey(idToEnable)) {
-                        nodeButtons.get(idToEnable).setStyle(getSkin().get("button-board-node-ability-enabled", ImageButton.ImageButtonStyle.class));
-                    } else {
-                        nodeButtons.get(idToEnable).setStyle(getSkin().get("button-board-node-enabled", ImageButton.ImageButtonStyle.class));
-                    }
+                    changeNodeStyle(idToEnable, NodeStatus.ENABLED);
                 }
 
-                Drawable drawable;
-                switch(e.orientation) {
-                    case VERTICAL:
-                        drawable = getSkin().getDrawable("ability-connection-vert-active");
-                        break;
-                    case UPLEFT:
-                        drawable = getSkin().getDrawable("ability-connection-diag-ul-dr-active");
-                        break;
-                    case UPRIGHT:
-                        drawable = getSkin().getDrawable("ability-connection-diag-ur-dl-active");
-                        break;
-                    default:
-                        drawable = getSkin().getDrawable("ability-connection-hor-active");
-                        break;
-                }
-                edgeImgs.get(e).setDrawable(drawable);
+                String imgName = edgeImgsDisabled[e.orientation] + "-active";
+                edgeImgs.get(e).setDrawable(getSkin().getDrawable(imgName));
             }
         }
     }
 
-    private void refreshDisplay() {
-        for(int node : team.get(0).activatedAbilityNodes) {
-            activateNode(node);
+    private void refreshDisplay(Monster monster) {
+        for(int node : monster.activatedAbilityNodes) {
+            changeNodeStyle(node, NodeStatus.ACTIVATED);
             enableNeighboringNodes(node);
         }
+    }
+
+    private void initBoard(Monster monster) {
+
+        nodeButtons.clear();
+        fieldGroup.clear();
+        circles.clear();
+        connections.clear();
+
+        Skin skin = getSkin();
+
+        for(AbilityGraph.Edge edge : graph.getEdges()) {
+            addConnection(skin, edge);
+        }
+
+        for(AbilityGraph.Vertex v : graph.getVertices().values()) {
+            // Add Circle
+            ImageButton circle;
+            if(MonsterInfo.getInstance().getStatusInfos().get(monster.ID).learnableAttacks.containsKey(v.ID)) {
+                circle = new ImageButton(disabledAbility);
+            } else {
+                circle = new ImageButton(disabled);
+            }
+            circle.setPosition(600/2-circle.getWidth()/2+32*v.x,300/2-circle.getHeight()/2+32*v.y, Align.bottomLeft);
+            circles.addActor(circle);
+            nodeButtons.put(v.ID, circle);
+            fieldGroup.add(circle);
+        }
+        nodeButtons.get(0).setChecked(true);
+
+        refreshDisplay(monster);
+
     }
 }

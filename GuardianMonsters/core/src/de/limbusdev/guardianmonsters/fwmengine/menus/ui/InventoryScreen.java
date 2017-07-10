dc3e481fa4c4a81f867078c48b8aa182be75c221
Georg Eckert <eckert.georg@gmx.de>
2017-07-11 01:07:53 +0200

    simplifies code in many places with lambdas
    Former-commit-id: 54e3164bcebe44f8ece2db55a243f2809251832f

--- GuardianMonsters/core/src/de/limbusdev/guardianmonsters/fwmengine/menus/ui/InventoryScreen.java
+++ GuardianMonsters/core/src/de/limbusdev/guardianmonsters/fwmengine/menus/ui/InventoryScreen.java
@@ -69,7 +69,8 @@ public class InventoryScreen implements Screen, MainToolBar.Callbacks
     }
 
     @Override
-    public void render(float delta) {
+    public void render(float delta)
+    {
         Gdx.gl.glClearColor(.1f, .1f, .1f, 1);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);

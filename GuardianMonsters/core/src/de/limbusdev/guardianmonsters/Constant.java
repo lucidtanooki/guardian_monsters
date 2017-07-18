b6fd59d89f313532a30edb2f2ccd9158a70860c6
Georg Eckert <eckert.georg@gmx.de>
2017-07-18 23:02:18 +0200

    changes Constant class to interface
    Former-commit-id: 7549f6c2abd9749303c270d4dd54c57af128ed38

--- GuardianMonsters/core/src/de/limbusdev/guardianmonsters/Constant.java
+++ GuardianMonsters/core/src/de/limbusdev/guardianmonsters/Constant.java
@@ -1,36 +1,32 @@
 package de.limbusdev.guardianmonsters;
 
-import de.limbusdev.guardianmonsters.model.gamestate.ForSerializationOnly;
 import de.limbusdev.guardianmonsters.utils.DebugMode;
 
 /**
  * Static container for Settings. Every parameter used in the game should be configured here
  * @author Georg Eckert 2017
  */
-public final class Constant {
+public interface Constant
+{
+    boolean LEFT = true;
+    boolean RIGHT = false;
 
-    @ForSerializationOnly
-    private Constant() {}
+    // ............................................................................................. ATTRIBUTES
+    int TILE_SIZE = 16;
+    int PIXELS_PER_METER = 1;
+    int RES_X = 428;
+    int RES_Y = 240;
+    int WIDTH = 428;
+    int HEIGHT = 240;
+    int COL = 8;
+    int ROW = 8;
+    int ONE_STEPDURATION_MS = 5;
+    int ONE_STEP_DURATION_PERSON = 20;
+    int zoom = 1;
 
-    public static final boolean LEFT = true;
-    public static final boolean RIGHT = false;
-
-    // .................................................................................. ATTRIBUTES
-    public static final int TILE_SIZE = 16;
-    public static final int PIXELS_PER_METER = 1;
-    public static final int RES_X = 428;
-    public static final int RES_Y = 240;
-    public static final int WIDTH = 428;
-    public static final int HEIGHT = 240;
-    public static final int COL = 8;
-    public static final int ROW = 8;
-    public static final int ONE_STEPDURATION_MS = 5;
-    public static final int ONE_STEP_DURATION_PERSON = 20;
-    public static final int zoom = 1;
-
-    // ................................................................................... DEBUGGING
-    public static final boolean DEBUGGING_ON = true;
-    public final static DebugMode DEBUG_MODE = DebugMode.WORLD;
-    public final static int startMap = 25;
-    public final static int startX = 1, startY = 1;
+    // ............................................................................................. DEBUGGING
+    boolean DEBUGGING_ON = true;
+    DebugMode DEBUG_MODE = DebugMode.WORLD;
+    int startMap = 25;
+    int startX = 1, startY = 1;
 }

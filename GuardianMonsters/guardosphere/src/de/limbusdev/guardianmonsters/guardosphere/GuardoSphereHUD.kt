fbeb440957a73beb3b9f4746d94acda4134aeffc
Georg Eckert <eckert.georg@gmx.de>
2019-02-12 11:27:49 +0100

    improves code readability

--- GuardianMonsters/guardosphere/src/de/limbusdev/guardianmonsters/guardosphere/GuardoSphereHUD.kt
+++ GuardianMonsters/guardosphere/src/de/limbusdev/guardianmonsters/guardosphere/GuardoSphereHUD.kt
@@ -1,6 +1,8 @@
 package de.limbusdev.guardianmonsters.guardosphere
 
-import com.badlogic.gdx.scenes.scene2d.ui.*
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.utils.Align
 import de.limbusdev.guardianmonsters.ui.Constant
 import de.limbusdev.guardianmonsters.assets.paths.AssetPath

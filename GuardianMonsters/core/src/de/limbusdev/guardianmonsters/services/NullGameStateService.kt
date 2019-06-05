d532b97e46c01ee6b79b9d6520c2dababc450fc7
Georg Eckert <eckert.georg@gmx.de>
2019-06-05 11:46:22 +0200

    m

--- GuardianMonsters/core/src/de/limbusdev/guardianmonsters/services/NullGameStateService.kt
+++ GuardianMonsters/core/src/de/limbusdev/guardianmonsters/services/NullGameStateService.kt
@@ -3,14 +3,11 @@ package de.limbusdev.guardianmonsters.services
 import de.limbusdev.guardianmonsters.fwmengine.managers.SaveGameManager
 import de.limbusdev.guardianmonsters.model.gamestate.GameState
 
-class NullGameStateService : IGameStateService {
+class NullGameStateService : IGameStateService
+{
     override fun destroy() {}
 
-    override fun getSaveGameManager(): SaveGameManager? {
-        return null
-    }
+    override fun getSaveGameManager(): SaveGameManager? = null
 
-    override fun getGameState(): GameState? {
-        return null
-    }
+    override fun getGameState(): GameState? = null
 }

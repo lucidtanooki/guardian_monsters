279106cd3b2da73354f5b3fb2d4686cd02c0aa78
Georg Eckert <eckert.georg@gmx.de>
2019-06-05 12:03:57 +0200

    Conversion to Kotlin: Node

---
+++ GuardianMonsters/guardians/src/de/limbusdev/guardianmonsters/guardians/abilities/Node.kt
@@ -0,0 +1,72 @@
+package de.limbusdev.guardianmonsters.guardians.abilities
+
+import com.badlogic.ashley.signals.Signal
+
+/**
+ * Node
+ *
+ * A *Node* is one of the base elements of the @see AbilityGraph. It represents some sort of ability
+ * a @see AGuardian can learn. It may also be in a specific state.
+ *
+ * @author Georg Eckert 2019
+ */
+
+class Node : Signal<Node>
+{
+    // ............................................................................... Inner Classes
+    enum class Type  { EMPTY, ABILITY, EQUIPMENT, METAMORPHOSIS }
+
+    enum class State { DISABLED, ENABLED, ACTIVE }
+
+    // .................................................................................. Properties
+    var x: Int = 0
+    var y: Int = 0
+    var ID: Int = 0
+    val type: Type
+    var state: State private set
+
+    // ................................................................................ Constructors
+    constructor(x: Int, y: Int, ID: Int)
+    {
+        this.y = y
+        this.x = x
+        this.ID = ID
+        this.type = Type.EMPTY
+        this.state = State.DISABLED
+    }
+
+    constructor(x: Int, y: Int, ID: Int, type: Type, state: State)
+    {
+        this.x = x
+        this.y = y
+        this.ID = ID
+        this.type = type
+        this.state = state
+    }
+
+    // ........................................................................... Getters & Setters
+    val isActive:   Boolean get() = state == State.ACTIVE;
+    val isEnabled:  Boolean get() = state == State.ENABLED
+    val isDisabled: Boolean get() = state == State.DISABLED
+
+
+    fun activate()
+    {
+        state = State.ACTIVE
+        dispatch(this)
+    }
+
+    fun enable()
+    {
+        if (state != State.ACTIVE) { state = State.ENABLED }
+        dispatch(this)
+    }
+
+    fun disable()
+    {
+        if (state != State.ACTIVE && state != State.ENABLED) { state = State.DISABLED }
+        dispatch(this)
+    }
+
+    override fun toString(): String = "Node $ID: $type & $state";
+}

23f058d6d1417a5f877227f4bc178fca58e3fd79
Georg Eckert <eckert.georg@gmx.de>
2019-06-10 15:52:26 +0200

    java2kotlin LevelUpWidget

---
+++ GuardianMonsters/battle/src/de/limbusdev/guardianmonsters/battle/ui/widgets/EndOfBattleWidget.kt
@@ -0,0 +1,79 @@
+package de.limbusdev.guardianmonsters.battle.ui.widgets
+
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.utils.Align
+
+import de.limbusdev.guardianmonsters.services.Services
+import de.limbusdev.guardianmonsters.ui.Constant
+import de.limbusdev.utils.extensions.f
+import ktx.actors.onClick
+
+/**
+ * Widget for displaying monster status in battle: HP, MP, EXP, Name, Level
+ * HINT: Don't forget calling the init() method
+ * Created by georg on 03.07.16.
+ */
+
+/**
+ *
+ * @param skin battle action UI skin
+ */
+class EndOfBattleWidget
+(
+        skin: Skin,
+        onBackButton: () -> Unit
+)
+    : BattleWidget()
+{
+    // .................................................................................. Properties
+    private val labelBGImg  : Image
+    private val bgImg       : Image
+    var messageLabel        : Label
+    var backButton          : ImageButton
+
+
+    // ................................................................................ Constructors
+    init
+    {
+        this.setBounds(0f, 0f, 0f, 0f)
+
+        labelBGImg = Image(skin.getDrawable("b-long-up"))
+        labelBGImg.setPosition(Constant.RES_X / 2f, Constant.ROW * 7f, Align.bottom)
+        bgImg = Image(skin.getDrawable("eob-pane"))
+        bgImg.setPosition(Constant.RES_X / 2f, 0f, Align.bottom)
+
+        addActor(labelBGImg)
+        addActor(bgImg)
+
+        val labs = Label.LabelStyle()
+        labs.font = skin.getFont("default-font")
+        messageLabel = Label("---", labs)
+        messageLabel.height = 64f
+        messageLabel.width = 500f
+        messageLabel.setWrap(true)
+        messageLabel.setPosition(Constant.RES_X / 2f, Constant.ROW * 8f, Align.bottom)
+        addActor(messageLabel)
+
+        // Change Screen
+        backButton = ImageButton(skin, "b-back-eob")
+        backButton.setPosition(Constant.RES_X.f(), 0f, Align.bottomRight)
+        addActor(backButton)
+
+        backButton.onClick(onBackButton)
+    }
+
+    /**
+     * true = hero won
+     * false = opponent won
+     * @param won
+     */
+    fun initialize(won: Boolean)
+    {
+        val i18n = Services.getL18N().Battle()
+        val message = if (won) "batt_game_over" else "batt_you_won"
+        messageLabel.setText(i18n.get(message))
+    }
+}
---
+++ GuardianMonsters/battle/src/de/limbusdev/guardianmonsters/battle/ui/widgets/LevelUpWidget.kt
@@ -0,0 +1,101 @@
+package de.limbusdev.guardianmonsters.battle.ui.widgets
+
+import com.badlogic.gdx.graphics.g2d.Animation
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.badlogic.gdx.utils.Align
+
+import de.limbusdev.guardianmonsters.guardians.monsters.AGuardian
+import de.limbusdev.guardianmonsters.guardians.monsters.LevelUpReport
+import de.limbusdev.guardianmonsters.scene2d.AnimatedImage
+import de.limbusdev.guardianmonsters.services.Services
+import de.limbusdev.guardianmonsters.ui.Constant
+import de.limbusdev.guardianmonsters.ui.widgets.OverlayWidget
+import ktx.actors.onClick
+
+/**
+ * LevelUpWidget
+ *
+ * @author Georg Eckert 2017
+ *
+ * TODO only show this at the end of a battle
+ */
+class LevelUpWidget(skin: Skin, guardian: AGuardian) : OverlayWidget(skin)
+{
+    private val monsterImg: Image
+
+    init
+    {
+        val bg = Label("", skin, "paper")
+        bg.setSize(300f, 180f)
+        bg.setPosition((Constant.WIDTH / 2 - 150).toFloat(), 30f, Align.bottomLeft)
+        addActor(bg)
+
+        monsterImg = Image(Services.getMedia().getMonsterSprite(guardian.speciesID, guardian.abilityGraph.currentForm))
+        monsterImg.setPosition(64f, 64f, Align.bottomLeft)
+        addActor(monsterImg)
+
+        val ok = ImageButton(skin, "button-back")
+        ok.setPosition((Constant.WIDTH - 64 - 4).toFloat(), (32 + 4).toFloat(), Align.bottomRight)
+        addActor(ok)
+        ok.onClick { remove() }
+
+        val guardianName = Services.getL18N().getGuardianNicknameIfAvailable(guardian)
+        val info = Label(Services.getL18N().Battle().format("level_up", guardianName), skin, "default")
+        info.setSize(140f, 32f)
+        info.setPosition((128 + 64).toFloat(), 140f, Align.bottomLeft)
+        info.setWrap(true)
+        info.setAlignment(Align.topLeft, Align.topLeft)
+        addActor(info)
+
+
+        // Values
+        val lvlUp = guardian.individualStatistics.latestLevelUpReport
+
+        val values = Table()
+        values.align(Align.topLeft)
+        values.setSize(140f, 72f)
+        values.setPosition(128f + 64f, 64f, Align.bottomLeft)
+
+        val attributes = arrayOf("exp", "hp", "mp", "pstr", "pdef", "mstr", "mdef", "speed")
+        val oldAttribVals = intArrayOf(
+                lvlUp!!.oldLevel,
+                lvlUp.oldStats.HP,
+                lvlUp.oldStats.MP,
+                lvlUp.oldStats.PDef,
+                lvlUp.oldStats.PStr,
+                lvlUp.oldStats.MStr,
+                lvlUp.oldStats.MDef,
+                lvlUp.oldStats.Speed)
+        val newAttribVals = intArrayOf(
+                lvlUp.newLevel,
+                lvlUp.newStats.HP,
+                lvlUp.newStats.MP,
+                lvlUp.newStats.PDef,
+                lvlUp.newStats.PStr,
+                lvlUp.newStats.MStr,
+                lvlUp.newStats.MDef,
+                lvlUp.newStats.Speed)
+
+        for (i in attributes.indices)
+        {
+            values.add(Image(skin.getDrawable("stats-symbol-" + attributes[i]))).size(16f, 16f)
+            values.add(Label(Integer.toString(oldAttribVals[i]) + " > ", skin, "default")).height(16f)
+            values.add(Label(Integer.toString(newAttribVals[i]), skin, "green")).height(16f).width(32f)
+            if (i % 2 == 1) values.row()
+        }
+
+        addActor(values)
+
+        val lvlUpAnimation = Animation(.15f, skin.getRegions("level-up-animation"))
+        lvlUpAnimation.playMode = Animation.PlayMode.LOOP
+        val ai = AnimatedImage(lvlUpAnimation)
+        ai.setPosition((128 + 32).toFloat(), (128 - 32).toFloat(), Align.topLeft)
+        addActor(ai)
+    }
+}

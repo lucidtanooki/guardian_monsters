d87bf3b52f5f5f6a8fc919900e5c8d67a23811c0
Georg Eckert <eckert.georg@gmx.de>
2019-07-03 14:01:08 +0200

    java2kotlin InventoryComponent

---
+++ GuardianMonsters/core/src/de/limbusdev/guardianmonsters/fwmengine/world/ecs/components/InventoryComponent.kt
@@ -0,0 +1,13 @@
+package de.limbusdev.guardianmonsters.fwmengine.world.ecs.components
+
+import com.badlogic.ashley.core.Component
+
+import de.limbusdev.guardianmonsters.guardians.items.Inventory
+
+/**
+ * InventoryComponent
+ *
+ * @author Georg Eckert 2017-02-17
+ */
+
+class InventoryComponent(var inventory: Inventory) : Component

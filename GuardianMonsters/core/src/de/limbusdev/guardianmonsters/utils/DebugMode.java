0d93cbd78830c516d59d27ac53679edf6a2a3cbb
Georg Eckert <eckert.georg@gmx.de>
2019-07-03 13:30:42 +0200

    java2kotlin DebugMode

--- GuardianMonsters/core/src/de/limbusdev/guardianmonsters/utils/DebugMode.java
+++
@@ -1,6 +0,0 @@
-package de.limbusdev.guardianmonsters.utils;
-
-public enum DebugMode {
-    RELEASE, WORLD, BATTLE, INVENTORY, WORLD_UI, BATTLE_SYSTEM, MONSTER_PARSING,
-    RESULT_SCREEN, METAMORPHOSIS, GUARDOSPHERE,
-}
---
+++ GuardianMonsters/core/src/de/limbusdev/guardianmonsters/utils/DebugMode.kt
@@ -0,0 +1,15 @@
+package de.limbusdev.guardianmonsters.utils
+
+enum class DebugMode
+{
+    RELEASE,
+    WORLD,
+    BATTLE,
+    INVENTORY,
+    WORLD_UI,
+    BATTLE_SYSTEM,
+    MONSTER_PARSING,
+    RESULT_SCREEN,
+    METAMORPHOSIS,
+    GUARDOSPHERE
+}

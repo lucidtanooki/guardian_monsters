34512be534f2475b33cb8b52cb2ec745820c1fd1
Georg Eckert <eckert.georg@gmx.de>
2019-09-25 16:43:37 +0200

    adds sprites to tiled people

---
+++ GuardianMonsters/android/assets/tilemaps/limbusdev_world2.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="world_day" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
+ <image source="../spritesheets/limbusdev_world2.png" width="1024" height="1024"/>
+ <tile id="3555">
+  <animation>
+   <frame tileid="3555" duration="100"/>
+   <frame tileid="3554" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
